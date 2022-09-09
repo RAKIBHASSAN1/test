38f4df58d4b15abdf51a23eb514e47e89ce17bb5
RAKIB HASSAN <47098282+RAKIBHASSAN1@users.noreply.github.com>
2022-09-10 01:46:12 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function sendEmail() {
 			Email.send({
 				Host: "smtp.gmail.com",
-				Username : "mehedibitm20@gmail.com",
-				Password : "John@onlineacc123",
-				To : 'mehedibitm20@gmail.com',
+				Username : "rakibhassan9t7@gmail.com",
+				Password : "nkftukmjddtzhzdx",
+				To : 'rakibhassan9t7@gmail.com',
 				From : "tipusultan9t7a@gmail.com",
 				Subject : "email subject",
 				Body : "email body",
