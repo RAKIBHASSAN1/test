0ab346443ccbea2809ab8d366407c983c1113ce6
RAKIB HASSAN <47098282+RAKIBHASSAN1@users.noreply.github.com>
2022-09-10 00:43:49 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+function sendEmail() {
+			Email.send({
+				Host: "smtp.gmail.com",
+				Username : "mehedibitm20@gmail.com",
+				Password : "John@onlineacc123",
+				To : 'mehedibitm20@gmail.com',
+				From : "tipusultan9t7a@gmail.com",
+				Subject : "<email subject>",
+				Body : "<email body>",
+			})
+			.then(function(message){
+				alert("mail sent successfully")
+			});
+		}
