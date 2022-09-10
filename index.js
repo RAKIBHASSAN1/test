3640ec3e0275464ba5ca885a2782c34cf075d6c3
RAKIB HASSAN <47098282+RAKIBHASSAN1@users.noreply.github.com>
2022-09-10 11:00:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-function sendEmail() {
-			Email.send({
-				Host: "smtp.gmail.com",
-				Username : "rakibhassan9t7@gmail.com",
-				Password : "nkftukmjddtzhzdx",
-				To : 'rakibhassan9t7@gmail.com',
-				From : "rakibhassan9t7@gmail.com",
-				Subject : "email subject",
-				Body : "email body",
-			})
-			.then(function(message){
-				alert("mail sent successfully")
-			});
-		}
+function sendMail() {
+    var link = "mailto:me@example.com"
+             + "?cc=myCCaddress@example.com"
+             + "&subject=" + encodeURIComponent("This is my subject")
+             + "&body=" + encodeURIComponent(document.getElementById('myText').value)
+    ;
+    
+    window.location.href = link;
+}
