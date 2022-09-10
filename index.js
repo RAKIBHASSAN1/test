86b198058565163812ecf506467aba16a3f8cf2b
RAKIB HASSAN <47098282+RAKIBHASSAN1@users.noreply.github.com>
2022-09-10 11:41:21 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function sendEmail() {
 			Email.send({
-				Host: "smtp.gmail.com",
-				Username : "rakibhassan9t7@gmail.com",
-				Password : "dsfhyacjvwsqlhit",
-				To : 'rakibhassan9t7@gmail.com',
-				From : "rakibhassan9t7@gmail.com",
-				Subject : "test mail",
-				Body : "hi",
+				SecureToken : "dsfhyacjvwsqlhit",
+                                To : 'rakibhassan9t7@gmail.com',
+                                From : "rakibhassan9t7@gmail.com",
+                                Subject : "This is the subject",
+                                Body : "And this is the body"
 			})
 			.then(function(message){
 				alert("mail sent successfully")
