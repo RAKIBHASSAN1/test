b1c7b40c5324265a3c834def037fc9508c74e6d7
RAKIB HASSAN <47098282+RAKIBHASSAN1@users.noreply.github.com>
2022-09-10 11:51:32 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sendEmail() {
 			Email.send({
-				SecureToken : "dsfhyacjvwsqlhit",
+				SecureToken : "6cbffe77-7788-4664-b2dc-33b3f06be059",
                                 To : 'rakibhassan9t7@gmail.com',
                                 From : "rakibhassan9t7@gmail.com",
                                 Subject : "This is the subject",
